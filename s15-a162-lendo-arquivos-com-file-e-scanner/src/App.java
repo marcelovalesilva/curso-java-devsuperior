3f7e552f28908a893f728ef1e4a8174b4eb6ee39
marcelovalesilva <marcelovale.s1@gmail.com>
2026-03-28 00:24:31 -0300

    s15: aulas 162 162 e 164

---
+++ s15-a162-lendo-arquivos-com-file-e-scanner/src/App.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        File file = new File("c:\\temp\\in.txt");
+        Scanner sc = null;
+        
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
